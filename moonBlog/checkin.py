5b85b3168c533d2c80848ba5d0cc7c5dab2b8564
BennyThink <benny.think@gmail.com>
2017-04-28 12:53:51 +0800

    邮件中包含主页

--- moonBlog/checkin.py
+++ moonBlog/checkin.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python
 # coding:utf-8
 
+import os
 import requests
 from bs4 import BeautifulSoup
 import re
-import os
 import yagmail
+
 
 def geturl(url):
     """
     function:get url of each post in the first page, 7 now.
     return: just an url, type list
     """
-    customheader={'User-Agent':'Mozilla/5.0 Sogou web spider'}
-    response = requests.get(url,headers=customheader)
+    customheader = {'User-Agent': 'Mozilla/5.0 Sogou web spider'}
+    response = requests.get(url, headers=customheader)
     soup1 = BeautifulSoup(response.text, 'html.parser')
-    titleAndUrl=soup1.find_all("article")
-    urlList=[]
+    titleAndUrl = soup1.find_all("article")
+    url_list = []
     for x in range(7):
-        #titleList.append( titleAndUrl[x].h3.text)
-        urlList.append(titleAndUrl[x].h3.a.get('href'))
-    return urlList
+        # titleList.append( titleAndUrl[x].h3.text)
+        url_list.append(titleAndUrl[x].h3.a.get('href'))
+    return url_list, response.text
 
 
 def download(url):
     customheader = {'User-Agent': 'Mozilla/5.0 Sogou web spider'}
     for x in range(7):
         response = requests.get(url[x], headers=customheader)
-        fileName=re.search(r'[0-9]{4}/.*.html',url[x]).group().replace('/','-')
+        fileName = re.search(r'[0-9]{4}/.*.html', url[x]).group().replace('/', '-')
         soup2 = BeautifulSoup(response.text, 'html.parser')
-        f=open('blog/'+fileName,'w')
+        f = open('blog/' + fileName, 'w')
         content = ''
         for string in soup2.find('div', class_='article-content clearfix').stripped_strings:
             content = content + string + '\n'
@@ -40,31 +41,24 @@ def download(url):
     return os.system('''cd blog && git add .&&git commit -m "`date`"''')
 
 
-def sendmail():
-    yag = yagmail.SMTP\
-        (user='you@email.com', password='your pass', host='smtp.exmail.qq.com', port='25')
-    yag.send(to="anonymous@qq.com", subject="明月博客已更新", contents='请使用git查看')
+def sendmail(html):
+    yag = yagmail.SMTP \
+        (user='no-reply@example.com', password='pass', host='smtp.exmail.qq.com', port='25')
+    yag.send(to="someone@qq.com", subject="博客已更新", contents=['主页预览：\n', html.encode('utf-8')])
 
 
 def main():
     print 'running program'
-    url = geturl('https://www.mingyueli.com')
+    url, html = geturl('https://www.mingyueli.com')
+    status = download(url)
 
-    if download(url) == 0:
+    if status == 0:
         print '更新了'
-        sendmail()
+        sendmail(html)
     else:
         print '保持原样'
 
 
 # main
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
