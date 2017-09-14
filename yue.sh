118274dc3b320184219be5569df79e44462f3ed8
Benny <benny.think@gmail.com>
2017-09-14 10:39:29 +0800

    syschart

--- yue.sh
+++ yue.sh
@@ -5,19 +5,19 @@ if [ -e "1.log" ]
 then
 	echo '1.log exists'
 else
-	curl https://www.mingyueli.com >1.log
+	curl https://www.tougetu.com.com >1.log
 fi
 
 #获取新内容
-curl https://www.mingyueli.com >2.log
+curl https://www.tougetu.com.com >2.log
 diff 1.log 2.log
 if [ $? -eq 0 ]
 then
 	echo "At `date` Everything stays the same"
 else
 	echo "At `date` It updated">> /root/yue.log
-	curl -d "diff=`diff 1.log 2.log |base64`" https://shemissed.me/mail.php
-	curl https://www.mingyueli.com >1.log
-	curl https://www.mingyueli.com >2.log
+	curl -d "diff=`diff 1.log 2.log |base64`" https://example.com/mail.php
+	curl https://www.tougetu.com.com >1.log
+	curl https://www.tougetu.com.com >2.log
 	
 fi
