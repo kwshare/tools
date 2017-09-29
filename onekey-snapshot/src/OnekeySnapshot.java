e8bc3087665be50ac5e8148589877487ab39195b
Benny <benny.think@gmail.com>
2017-09-29 11:30:40 +0800

    一键快照 删除最旧的而不是上一个

--- onekey-snapshot/src/OnekeySnapshot.java
+++ onekey-snapshot/src/OnekeySnapshot.java
@@ -2,6 +2,7 @@
 import com.qcloud.Module.Snapshot;
 import com.qcloud.QcloudApiModuleCenter;
 import com.qcloud.Utilities.Json.JSONObject;
+
 import java.util.TreeMap;
 
 /*
@@ -9,16 +10,16 @@ import java.util.TreeMap;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 /**
- *
  * @author Benny~
  */
 public class OnekeySnapshot {
 
-    private static String secretID = "AKIDU---EQgAgQ2";
-    private static String secretKey = "nV2OY---qqkmi";
-    private static String Region = "sh";
-    private static String diskID = "disk-ixath";
+    private static String secretID = "Your secretID";
+    private static String secretKey = "Your secretKey";
+    private static String Region = "Your Region";
+    private static String diskID = "Your diskID";
     private static String SnapshotID = null;
 
     public static void main(String[] args) {
@@ -29,16 +30,21 @@ public class OnekeySnapshot {
 
     private static void createSnapShot() {
         //创建快照
+
         TreeMap<String, Object> config = new TreeMap<String, Object>();
         config.put("SecretId", secretID);
         config.put("SecretKey", secretKey);
         config.put("RequestMethod", "GET");
         config.put("DefaultRegion", Region);
         QcloudApiModuleCenter module = new QcloudApiModuleCenter(new Snapshot(),
-            config);
+                config);
         TreeMap<String, Object> params = new TreeMap<String, Object>();
         params.put("limits", 3);
         params.put("storageId", diskID);
+        //Magic snapshot name
+        String[] fortune = {"I miss you", "I missed you", "Exodus", "I flipped", "Nothing will ever compare",
+                "Remember me", "Benny with Moon"};
+        params.put("snapshotName", fortune[(int) (Math.random() * 7)]);
 
         //System.out.println(module.generateUrl(Action,params));
         String result = null;
@@ -54,14 +60,14 @@ public class OnekeySnapshot {
     }
 
     private static void processSnapShot() {
-        //查询快照，存储上一个的snapshotID,并删除上一个快照
+        //查询快照，存储旧的snapshotID,并删除旧快照
         TreeMap<String, Object> config = new TreeMap<String, Object>();
         config.put("SecretId", secretID);
         config.put("SecretKey", secretKey);
         config.put("RequestMethod", "GET");
         config.put("DefaultRegion", Region);
         QcloudApiModuleCenter module = new QcloudApiModuleCenter(new Snapshot(),
-            config);
+                config);
         TreeMap<String, Object> params = new TreeMap<String, Object>();//instanceIds.0
         params.put("limits", 3);
         params.put("storageId", diskID);
@@ -71,15 +77,16 @@ public class OnekeySnapshot {
         try {
             result = module.call("DescribeSnapshots", params);
             JSONObject json_result = new JSONObject(result);
-            //取得最后一个快照的ID          
-            SnapshotID = json_result.getJSONArray("snapshotSet").getJSONObject(json_result.getInt("totalCount") - 1).getString("snapshotId");
-            //删除最后一个快照
+            //取得最旧的快照ID
+
+            SnapshotID = json_result.getJSONArray("snapshotSet").getJSONObject(0).getString("snapshotId");
+            //删除最旧的快照
             params.clear();
             params.put("limits", 3);
             params.put("snapshotIds.0", SnapshotID);
             result = module.call("DeleteSnapshot", params);
             json_result = new JSONObject(result);
-            System.out.println("删除最后一个快照中..." + json_result.getString("codeDesc"));
+            System.out.println("删除最旧一个快照中..." + json_result.getString("codeDesc"));
 
         } catch (Exception e) {
             System.out.println("error..." + e.getMessage());
