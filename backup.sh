7def9ff251b86a62cffd370af60b0df3bed273cc
Benny <benny.think@gmail.com>
2017-10-10 10:24:10 +0800

    execlude and seperate backups

--- backup.sh
+++ backup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-#用于备份数据库、备份网站文件并上传至七牛
-#需要其他工具的配合
-#建议配合cron
 echo ***********`date`  backup is staring************ >>/home/wwwlogs/backup.log
+#Benny
+mysqldump -u root -p wordpress --password=Y311 > /home/wwwroot/www.bennythink.com/db_backup/`date +%Y%m%d`.sql
+tar -jcpf /home/wwwbackup/`date +%Y%m%d`_benny.tar.bz2 /home/wwwroot/www.bennythink.com/ --exclude=/home/wwwroot/www.bennythink.com/phpmyadmin >/dev/null 2>&1
+#love
+mysqldump -u root -p hi --password=Y311 > /home/wwwroot/www.tougetu.com/db_backup/`date +%Y%m%d`.sql
+tar -jcpf /home/wwwbackup/`date +%Y%m%d`_moon.tar.bz2 /home/wwwroot/www.tougetu.com/ >/dev/null 2>&1
 
-mysqldump -u root -p wordpress --password=yourpass > /home/wwwroot/www.bennythink.com/phpmyadmin/upload/`date +%Y%m%d`.sql
-tar -jcpf /home/wwwbackup/`date +%Y%m%d`.tar.bz2 /home/wwwroot/ >/dev/null 2>&1
 #using qshell
-rm -r /home/.qshell
+rm -r /root/.qshell
 qshell qupload /home/qiniu/backup.json >>/home/wwwlogs/backup.log
 echo 'qiniu upload succeed'>>/home/wwwlogs/backup.log
 #using cos
@@ -15,9 +16,9 @@ cd /home/cos && bash start_cos_sync.sh >>/home/wwwlogs/backup.log
 echo 'cos upload succeed'>>/home/wwwlogs/backup.log
 #using Google Drive
 grive -up /home/wwwbackup >>/home/wwwlogs/backup.log
-echo 'Google Drive upload succeed'>>/home/wwwlogs/backup.log
 #delete files
-rm /home/wwwroot/www.bennythink.com/phpmyadmin/upload/*
+rm /home/wwwroot/www.tougetu.com/db_backup/*
+rm /home/wwwroot/www.bennythink.com/db_backup/*
 rm /home/wwwbackup/*
 
 echo End of backup at `date` >>/home/wwwlogs/backup.log
