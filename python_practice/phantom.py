27a1a187867d67e0a5d1d4801bff59f4410579e7
Benny <benny.think@gmail.com>
2017-11-22 11:04:19 +0800

    selenium

--- python_practice/phantom.py
+++ python_practice/phantom.py
@@ -8,11 +8,14 @@
 __author__ = 'Benny <benny@bennythink.com>'
 
 from selenium import webdriver
-
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
 driver = webdriver.PhantomJS()
 driver.get('https://www.bennythink.com/ubuntumate-raspberry2.html')
 driver.find_element_by_xpath('//*[@id="Addlike"]').click()
 # this doesn't work..?
 driver.delete_all_cookies()
+driver.page_source()
 driver.quit()
 
