9bd0de2b6e926a936e9e6df58dbfef38bcbfdd68
Benny <benny.think@gmail.com>
2018-01-30 10:41:08 +0800

    password regex

--- python_practice/regex.py
+++ python_practice/regex.py
@@ -5,14 +5,8 @@
 
 import re
 
+regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[~!@#$%^&*])[\S]{8,16}$')
 
-p = re.compile(r'\d+')
-print p.findall('one1two2three3four4')
+s = '111csdaC@11'
 
-pattern = re.compile(r'world')
-
-# 使用search()查找匹配的子串，不存在能匹配的子串时将返回None
-# 这个例子中使用match()无法成功匹配
-match = pattern.search('hello world!')
-
-print match.group()
+print regex.findall(s)
