382081b03e4b0c1761d994450f7ea9e5916c6b57
Benny <benny.think@gmail.com>
2018-05-31 21:59:20 +0800

    ca chart

---
+++ chart/nova.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# coding:utf-8
+
+# tools - nova.py
+# 2018/5/31 21:08
+# 
+
+__author__ = 'Benny <benny@bennythink.com>'
+
+import plotly
+import plotly.graph_objs as go
+import json
+
+
+def draw(x, y, fn):
+    data = [go.Bar(
+        x=x,
+        y=y
+    )]
+    layout = go.Layout(title='Status Report', )
+
+    fig = go.Figure(data=data, layout=layout)
+    plotly.offline.plot(fig, data, filename='%s.html' % fn)
+
+
+def parse():
+    j = json.load(open('status.json'))
+    ":type:dict"
+
+    x = []
+    y = []
+    for date in j:
+        x.append(date)
+        y.append(j.get(date, {}).get('count', 0))
+    return x, y
+
+
+if __name__ == '__main__':
+    x, y = parse()
+    draw(x, y, 'What')
