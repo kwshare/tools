1c98c2ec25519aa39fa782b5772b8f835a2dc88a
Benny <benny.think@gmail.com>
2018-12-06 14:28:26 +0800

    Create nginx-tls1.3.sh

---
+++ nginx-tls1.3.sh
@@ -0,0 +1,1 @@
+./configure --user=www --group=www  --prefix=/usr/local/nginx --add-module=../ngx_brotli --with-openssl=../openssl --with-openssl-opt='enable-tls1_3 enable-weak-ssl-ciphers' --with-http_v2_module --with-http_ssl_module --with-http_gzip_static_module --with-http_stub_status_module --with-http_sub_module --with-stream --with-stream_ssl_module
