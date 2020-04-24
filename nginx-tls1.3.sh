da0fe09b6a7aeaadb6f1becf6e4c70c42dd94d5f
Benny~ <benny.think@gmail.com>
2020-04-24 13:11:52 +0800

    update

--- nginx-tls1.3.sh
+++ nginx-tls1.3.sh
@@ -1,1 +1,7 @@
-./configure --user=www --group=www  --prefix=/usr/local/nginx --add-module=../ngx_brotli --with-openssl=../openssl --with-openssl-opt='enable-tls1_3' --with-http_v2_module --with-http_ssl_module --with-http_gzip_static_module --with-http_stub_status_module --with-http_sub_module --with-stream --with-stream_ssl_module
+./configure --user=www --group=www  --prefix=/usr/local/nginx \
+--add-module=../ngx_brotli --with-openssl=../openssl --with-openssl-opt='enable-tls1_3' \
+--with-http_v2_module --with-http_ssl_module --with-http_gzip_static_module \
+--with-http_stub_status_module --with-http_sub_module --with-stream \
+--with-stream_ssl_module\
+--with-compat
+
