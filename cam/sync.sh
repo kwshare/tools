e9dc35240eb7c14f5d4544361dcd3a3ee14c4fcc
Benny <benny.think@gmail.com>
2020-07-16 22:59:37 +0800

    Update sync.sh

--- cam/sync.sh
+++ cam/sync.sh
@@ -4,8 +4,6 @@
 
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 
-day=$(date +"%d")
-
 proxychains4 -q rsync -avz --delete /video/ user@host:/video/
 
 cmd="find /video -mindepth 1 -mtime +30 -delete"
