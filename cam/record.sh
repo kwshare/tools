a3680ed920481fd00ed32a24149260a4e8aa7402
Benny <benny.think@gmail.com>
2020-07-16 22:59:06 +0800

    Update record.sh

--- cam/record.sh
+++ cam/record.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+export PATH=/root/.autojump/bin:/root/.autojump/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 
-today=$(date +"%Y-%m-%d")
-RTSP="rtsp://127.0.0.1:554/user=user&password=password&channel=Channel&stream=Stream.sdp?real_stream"
-
-ffmpeg  -rtsp_transport tcp  -i $RTSP -vcodec  copy -r 1 -t 43200  -y /video/${today}.mp4
+now=$(date +"%Y-%m-%d_%H-%M")
+RTSP="rtsp://192.168.7.234:554/user=admin&password=DontTouchMe&channel=Channel&stream=Stream.sdp?real_stream"
+ffmpeg  -rtsp_transport tcp  -i $RTSP -vcodec  copy -r 1 -t 890  -y /video/${now}.mp4
