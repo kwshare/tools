1a2bf7f99c3461f495e3cf0f7ac0c25d3d47980e
Benny <benny.think@gmail.com>
2020-07-17 09:23:08 +0800

    Update sync.sh

--- cam/sync.sh
+++ cam/sync.sh
@@ -4,9 +4,8 @@
 
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 
+cmd="find /video -mindepth 1 -mtime +30 -delete"
+${cmd}
 proxychains4 -q rsync -avz --delete /video/ user@host:/video/
 
-cmd="find /video -mindepth 1 -mtime +30 -delete"
-
-${cmd}
-proxychains4 -q ssh user@host ${cmd}
+# proxychains4 -q ssh user@host ${cmd}
