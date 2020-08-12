65f6afd4a94aaca25e9804880e3c8701479b2450
Benny <benny.think@gmail.com>
2020-08-12 17:38:40 +0800

    Update nginx-tls1.3.sh

--- nginx-tls1.3.sh
+++ nginx-tls1.3.sh
@@ -1,8 +1,31 @@
-./configure --user=www --group=www  --prefix=/usr/local/nginx \
---add-module=../ngx_brotli --with-openssl=../openssl --with-openssl-opt='enable-tls1_3' \
---with-http_v2_module --with-http_ssl_module --with-http_gzip_static_module \
---with-http_stub_status_module --with-http_sub_module --with-stream \
---with-stream_ssl_module \
---with-compat \
---with-http_realip_module
+mkdir build && cd build
+
+wget  https://nginx.org/download/nginx-1.19.2.tar.gz
+tar xf nginx-1.19.2.tar.gz
+
+git clone https://github.com/google/ngx_brotli
+cd ngx_brotli && git submodule update --init && cd ..
+cd nginx-1.19.2
+openssl version|grep 1.1.1
+
+if [ $? -eq 0 ];then
+    ./configure --user=www --group=www  --prefix=/usr/local/nginx \
+    --add-module=../ngx_brotli  --with-openssl-opt='enable-tls1_3' \
+    --with-http_v2_module --with-http_ssl_module --with-http_gzip_static_module \
+    --with-http_stub_status_module --with-http_sub_module --with-stream \
+    --with-stream_ssl_module \
+    --with-compat \
+    --with-http_realip_module
+  else
+    git clone https://github.com/openssl/openssl
+      ./configure --user=www --group=www  --prefix=/usr/local/nginx \
+    --add-module=../ngx_brotli --with-openssl=../openssl --with-openssl-opt='enable-tls1_3' \
+    --with-http_v2_module --with-http_ssl_module --with-http_gzip_static_module \
+    --with-http_stub_status_module --with-http_sub_module --with-stream \
+    --with-stream_ssl_module \
+    --with-compat \
+    --with-http_realip_module
+fi
+
+
 
